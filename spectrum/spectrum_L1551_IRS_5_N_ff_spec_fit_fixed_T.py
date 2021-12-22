79a1a9b3ca526031328d819143b4829cd54cff8c
Anton <anton.fj22@gmail.com>
2021-12-22 23:10:54 +0000

    Changed size of plots to fit in paper

--- spectrum/spectrum_L1551_IRS_5_N_ff_spec_fit_fixed_T.py
+++ spectrum/spectrum_L1551_IRS_5_N_ff_spec_fit_fixed_T.py
@@ -7,7 +7,7 @@ import scipy.optimize
 from uncertainties import ufloat
 from uncertainties.umath import *  # Imports sin(), etc.
 
-T_e = 2e4                               # Fixed value of electron temperature
+T_e = 1e4                               # Fixed value of electron temperature
 #theta_maj = ufloat(0.396, 0.023)	# Major axis of source in arcsec
 #theta_min = ufloat(0.057, 0.022) 	# Minor axis of source in arcsec
 theta_maj = 0.202               	# Major axis of source in arcsec
@@ -170,11 +170,11 @@ matplotlib.rcParams['ytick.direction'] = 'in'
 # Makes all text non-italic
 matplotlib.rcParams.update({'mathtext.default': 'regular'})
 # Sets font size of all text on graph
-matplotlib.rcParams.update({'font.size': 16})
-plt.rc('legend', fontsize=16)
+matplotlib.rcParams.update({'font.size': 12})
+plt.rc('legend', fontsize=12)
 
 # Plot data values
-fig = plt.figure(5, figsize=(15, 9))
+fig = plt.figure(5, figsize=(7, 4))
 ax = fig.add_subplot(111)
 plt.errorbar(log_freq, log_flux, yerr=log_flux_err, fmt='bo', label='2020')
 # Plot 2003 data

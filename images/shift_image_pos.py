0eaa14e9779225dca2c38ce1744735b3ff2f2963
antonfj <antonfj@cp.dias.ie>
2022-03-27 14:12:44 +0100

    Fixed position of ALMA band 6

--- images/shift_image_pos.py
+++ images/shift_image_pos.py
@@ -1,14 +1,14 @@
 from astropy.io import fits
 
-image_file='L1551_IRS_5_ALMA_Band_3_cropped.cont.I.manual.image.pbcor.fits'
-output_file='L1551_IRS_5_ALMA_Band_3_cropped_shifted_to_VLA_pos.image.pbcor.fits'
+image_file='L1551_IRS_5_ALMA_Band_6.fits'
+output_file='L1551_IRS_5_ALMA_Band_6_shifted.fits'
 
 image_data, image_header = fits.getdata(image_file, header=True)
 #print(image_header)
 
 # RA and Dec shifts in milliarcsec
-ra_shift = -55.16
-dec_shift = 57.69
+ra_shift = -60.9
+dec_shift = 63.7
 
 # Convert to degrees
 ra_shift = ra_shift/(1000*60*60)

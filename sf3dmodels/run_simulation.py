d3f16738d9997ec79518a136c09e83b2df74c0c2
antonfj <antonfj@cp.dias.ie>
2023-01-30 16:28:03 +0000

    Changed image simulation script to conical jet

--- sf3dmodels/run_simulation.py
+++ sf3dmodels/run_simulation.py
@@ -30,7 +30,7 @@ from matplotlib import cm
 t0 = time.time()
 #--------------------
 
-dx_grid = 0.25*u.au
+dx_grid = 0.5*u.au
 #*********************
 #OUTFLOW 1
 #*********************
@@ -42,7 +42,7 @@ tag = '_outflow1'
 pos_c = np.array([0*u.au, 0*u.au, 0])
 axis = np.array([-1,0.7,-1.22]) 
 # Min and max distances to calculate jet parameters
-z_min = 10*u.au
+z_min = 10.0*u.au
 z_max = 120*u.au
 
 z0 = z_min               # Injection radius of jet
@@ -55,7 +55,7 @@ op_angle = 0.489         # Opening angle of jet in radians
 #w0 = 1*u.au
 w0 = z0 * np.tan(op_angle/2)
 print("w0 (au): ", w0/u.au)
-eps = 0.66
+eps = 1.0
 w = [w0, eps]
 
 T0 = 1e4
@@ -69,7 +69,7 @@ gtd = 100
 
 v0 = 200 * 1e3 #km/s
 
-mass_loss_rate=4.7e-9     # Mass loss rate of star in M_sun yr^-1
+mass_loss_rate=1e-9     # Mass loss rate of star in M_sun yr^-1
 
 #dens0 = 2e12
 dens0 = (1.2e49/(w0**2 * v0)) * mass_loss_rate # Calculate initial density from mass loss rate
@@ -111,7 +111,7 @@ t0 = time.time()
 #********
 sizex = 200 * u.au
 sizey = sizez = 200 * u.au
-Nx = Ny = Nz = 800
+Nx = Ny = Nz = 400
 GRID = Model.grid([sizex, sizey, sizez], [Nx, Ny, Nz], rt_code='radmc3d')
 
 #********
@@ -163,9 +163,9 @@ t0 = time.time()
 num_threads = "24"      # Set num. of threads for RADMC-3D to use
 
 # Image at C Band (5 GHz)
-frequency = 5e9         # Frequency to simulate image
+frequency = 10e9        # Frequency to simulate image
 sizeau = "270"          # Size in au of area to image
-npix = "100"             # Size of image in pixels
+npix = "400"            # Size of image in pixels
 
 # Image at C Band (5 GHz)
 radmc3d_command = ["radmc3d", "image", "lambda", "60000", "setthreads", num_threads, "sizeau", sizeau, "npix", npix, "stokes"]
@@ -179,15 +179,18 @@ print ('-------------------------------------------------\n')
 print ('Ellapsed time: %.3fs' % (time.time() - t0))
 print ('-------------------------------------------------\n-------------------------------------------------\n')
 
-C_Band_image = readImage("image.out")
+X_Band_image = readImage("image.out")
+X_Band_image.writeFits(fname="L1551_IRS_5_C_Band_5_GHz_simulated_conical_mlr_1e-9_z0_10au_zmax_120au_res_0.5au_grid_0.5au_no_conv.fits", dpc=140,coord="04h31m34.17s+18d08m04.28s")
 # Convolve beam of e-MERLIN C Band image
-C_Band_image = C_Band_image.imConv(dpc=140.,psfType='gauss',fwhm=[0.220,0.113],pa=43.831)
+#X_Band_image = X_Band_image.imConv(dpc=140.,psfType='gauss',fwhm=[0.20,0.18],pa=-49.0)
+X_Band_image = X_Band_image.imConv(dpc=140.,psfType='gauss',fwhm=[0.22,0.11],pa=44.0)
 #plotImage(C_Band_image,log=False,cmap=cm.hot,bunit='jy/beam',dpc=140,au=False,arcsec=True)
 
 #Create fits file of image at coordinates of L1551 IRS 5
-C_Band_image.writeFits(fname="L1551_IRS_5_C_Band_5_GHz_simulated_mlr_4.7e-9_z0_10au_zmax_120au_res_0.25au_grid_0.25au_coarse.fits", dpc=140,coord="04h31m34.17s+18d08m04.28s",
+X_Band_image.writeFits(fname="L1551_IRS_5_C_Band_5_GHz_simulated_conical_mlr_1e-9_z0_10au_zmax_120au_res_0.5au_grid_0.5au.fits", dpc=140,coord="04h31m34.17s+18d08m04.28s",
+#                       bandwidthmhz=500.0, casa=False,stokes="I")
+#X_Band_image.writeFits(fname="L1551_IRS_5_C_Band_5_GHz_simulated_test.fits", dpc=140,coord="04h31m34.17s+18d08m04.28s",
                        bandwidthmhz=500.0, casa=False,stokes="I")
-
 """
 # Same image convolved to X Band beam
 C_Band_image = readImage("image.out")
